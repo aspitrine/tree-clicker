c80d74f2305d160bebfe890ef5171e8f5cf687f1
Aspitrine <loquente62@gmail.com>
2024-02-08 12:31:57 +0100

    ci: :sparkles: deploy on github page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,14 @@
+import process from 'node:process'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@vueuse/nuxt'],
   ui: {
     icons: ['mdi'],
+  },
+  app: {
+    baseURL: process.env.BASE_URL || '',
+    buildAssetsDir: `${process.env.BASE_URL || ''}/_nuxt/`,
   },
 })
