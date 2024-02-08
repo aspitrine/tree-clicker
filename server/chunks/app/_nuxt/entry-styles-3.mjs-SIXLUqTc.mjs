22098cf47f1f8340df65f68a29730bb55a1bf804
aspitrine <aspitrine@users.noreply.github.com>
2024-02-08 13:38:42 +0000

    deploy: c8b80d2024fd61a9f078b88fe61c53f6623819d5

--- server/chunks/app/_nuxt/entry-styles-3.mjs-SIXLUqTc.mjs
+++
@@ -1,4 +0,0 @@
-const Icon_vue_vue_type_style_index_0_scoped_adda1fdc_lang = ".icon[data-v-adda1fdc]{display:inline-block;vertical-align:middle}";
-
-export { Icon_vue_vue_type_style_index_0_scoped_adda1fdc_lang as I };
-//# sourceMappingURL=entry-styles-3.mjs-SIXLUqTc.mjs.map
